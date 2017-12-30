1e3b4824c8b633e49acbdc69f7efef6c3c169686
Nate Cheng <boo0330@gmail.com>
2017-12-29 16:34:33 -0800

    init

---
+++ server/controllers/auth/middleware/auth.js
@@ -0,0 +1,29 @@
+var jwt = require('jsonwebtoken');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../../../config')[env];
+
+module.exports = {
+
+  verifyToken: ( (req, res, next) => {
+      let token = req.body.token || req.query.token || req.headers['x-access-token'];
+      //console.log(token);
+      if( token ) {
+
+          jwt.verify(token, config.secret, (err, decoded) => {
+  
+              if (err) {
+                  return res.json({ success: false, message: 'Failed to authenticate token.' });    
+              } else {
+                  // all good, continue
+                  req.decoded = decoded; 
+                  next();
+              }
+          });
+
+      }  else {
+
+          res.send({ success: false, message: 'No token exists.' });
+          
+      }
+  })
+}
---
+++ server/controllers/users/users.controller.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var users_router = express.Router();
+var user_router = express.Router();
+// define the home page route
+users_router.get('/', function (req, res) {
+  res.send('users list')
+})
+
+
+
+
+user_router.get('/:userId', function (req, res) {
+  res.send('get user: '+ req.params.userId);
+})
+module.exports = {
+  users: users_router,
+  user: user_router
+}
